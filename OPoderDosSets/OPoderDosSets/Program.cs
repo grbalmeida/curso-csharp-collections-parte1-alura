4acd44bf06ad244b88ff78c72d949a5b8a01e89b
gilvan <g.r.almeida@live.com>
2020-09-13 03:40:31 -0300

    O Poder dos Sets

---
+++ OPoderDosSets/OPoderDosSets/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace OPoderDosSets
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //SETS = CONJUNTOS
+
+            //Duas propriedades do Set
+            //1. não permite duplicidade
+            //2. os elementos não são mantidos em ordem específicas
+
+            ISet<string> alunos = new HashSet<string>();
+
+            alunos.Add("Vanessa Tonini");
+            alunos.Add("Ana Losnak");
+            alunos.Add("Rafael Nercessian");
+
+            Console.WriteLine(alunos);
+            Console.WriteLine(string.Join(",", alunos));
+
+            alunos.Add("Priscila Stuani");
+            alunos.Add("Rafael Rollo");
+            alunos.Add("Fabio Gushiken");
+
+            Console.WriteLine(string.Join(",", alunos));
+
+            alunos.Remove("Ana Losnak");
+            alunos.Add("Marcelo Oliveira");
+
+            Console.WriteLine(string.Join(",", alunos));
+
+            alunos.Add("Fabio Gushiken");
+
+            Console.WriteLine(string.Join(",", alunos));
+
+            // qual a vantagem do set sobre a lista?
+            // conjuntos são mais rápidos em buscar elementos
+            // HashSet possui grande escalabilidade
+            // Ponto fraco do HashSet é o consumo de memória ao lidar com grande quantidade de elementos
+
+            // alunos.Sort()
+
+            List<string> alunosEmLista = new List<string>(alunos);
+
+            alunosEmLista.Sort();
+
+            Console.WriteLine(string.Join(",", alunosEmLista));
+
+            Console.ReadLine();
+        }
+    }
+}
