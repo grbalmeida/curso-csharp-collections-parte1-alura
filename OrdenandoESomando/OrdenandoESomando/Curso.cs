025f251532772514be3f2355c4bfecc45f2f624b
gilvan <g.r.almeida@live.com>
2020-09-12 22:47:56 -0300

    Ordenando e Totalizando

---
+++ OrdenandoESomando/OrdenandoESomando/Curso.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+
+namespace OrdenandoESomando
+{
+    public class Curso
+    {
+        private IList<Aula> aulas;
+        private string nome;
+        private string instrutor;
+
+        public Curso(string nome, string instrutor)
+        {
+            this.nome = nome;
+            this.instrutor = instrutor;
+            aulas = new List<Aula>();
+        }
+
+        public void Adiciona(Aula aula)
+        {
+            aulas.Add(aula);
+        }
+
+        public IList<Aula> Aulas
+        {
+            get { return new ReadOnlyCollection<Aula>(aulas); }
+        }
+
+
+        public string Nome
+        {
+            get { return nome; }
+            set { nome = value; }
+        }
+
+
+        public string Instrutor
+        {
+            get { return instrutor; }
+            set { instrutor = value; }
+        }
+
+        public int TempoTotal
+        {
+            get
+            {
+                //int total = 0;
+
+                //foreach (var aula in aulas)
+                //{
+                //    total += aula.Tempo;
+                //}
+
+                //return total;
+
+                // LINQ = Language Integrated Query
+                // Consulta Integrada à Linguagem
+
+                return aulas.Sum(aula => aula.Tempo);
+            }
+        }
+
+        public override string ToString()
+        {
+            return $"Curso: {nome}, Tempo: {TempoTotal}, Aulas: {string.Join(",", aulas)}";
+        }
+
+    }
+}
---
+++ OrdenandoESomando/OrdenandoESomando/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace OrdenandoESomando
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Curso csharpColecoes = new Curso("C# Collections", "Marcelo Oliveira");
+            csharpColecoes.Adiciona(new Aula("Trabalhando com Listas", 21));
+            // Ao tentar adicionar uma nova aula em uma lista somente leitura lança NotSupportedException
+
+            Imprimir(csharpColecoes.Aulas);
+
+            // adicionar 2 aulas
+            csharpColecoes.Adiciona(new Aula("Criando uma Aula", 20));
+            csharpColecoes.Adiciona(new Aula("Modelando com Coleções", 19));
+
+            // imprimir
+            Imprimir(csharpColecoes.Aulas);
+
+            // ordenar a lista de aulas
+            // csharpColecoes.Aulas.Sort();
+
+            // copiar a lista para outra lista
+            List<Aula> aulasCopiadas = new List<Aula>(csharpColecoes.Aulas);
+
+            // ordenar a cópia
+            aulasCopiadas.Sort();
+
+            Imprimir(aulasCopiadas);
+
+            // totalizar o tempo do curso
+            Console.WriteLine(csharpColecoes.TempoTotal);
+
+            Console.WriteLine(csharpColecoes);
+
+            Console.ReadLine();
+        }
+
+        private static void Imprimir(IList<Aula> aulas)
+        {
+            Console.Clear();
+
+            foreach (var aula in aulas)
+            {
+                Console.WriteLine(aula);
+            }
+        }
+    }
+}
