68a48ba3536314bf610e9d11b25d2a9e98eafedc
gilvan <g.r.almeida@live.com>
2020-09-13 04:19:23 -0300

    Equals e HashCode

---
+++ EqualsEHashCode/EqualsEHashCode/Aluno.cs
@@ -0,0 +1,56 @@
+﻿namespace EqualsEHashCode
+{
+    public class Aluno
+    {
+        private string nome;
+        private int numeroMatricula;
+
+        public Aluno(string nome, int numeroMatricula)
+        {
+            this.nome = nome;
+            this.numeroMatricula = numeroMatricula;
+        }
+
+        public string Nome
+        {
+            get { return nome; }
+            set { nome = value; }
+        }
+
+        public int NumeroMatricula
+        {
+            get { return numeroMatricula; }
+            set { numeroMatricula = value; }
+        }
+
+        public override string ToString()
+        {
+            return $"[Nome: {nome}, Matrícula: {numeroMatricula}]";
+        }
+
+        public override bool Equals(object obj)
+        {
+            Aluno outro = obj as Aluno;
+
+            if (outro == null)
+            {
+                return false;
+            }
+
+            return nome.Equals(outro.Nome);
+        }
+
+        // Alunos são convertidos internamente no HashSet para códigos
+        // Os códigos vão cair numa tabela de dispersão, também chamada de HashTable
+        // A tabela de dispersão diz em que lugar o conjunto irá ficar
+        // importante: rapidez da busca depende do CÓDIGO DE DISPERSÃO!
+        // Dois objetos que são iguais possuem o mesmo hash code.
+        // PORÉM, o contrário não é verdadeiro:
+        // Dois objetos com mesmo hash codes não são necessariamente iguais!
+        public override int GetHashCode()
+        {
+            return nome.GetHashCode();
+        }
+
+    }
+}
---
+++ EqualsEHashCode/EqualsEHashCode/Curso.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+
+namespace EqualsEHashCode
+{
+    public class Curso
+    {
+        private ISet<Aluno> alunos = new HashSet<Aluno>();
+        private IList<Aula> aulas;
+        private string nome;
+        private string instrutor;
+
+        public Curso(string nome, string instrutor)
+        {
+            this.nome = nome;
+            this.instrutor = instrutor;
+            aulas = new List<Aula>();
+        }
+
+        public IList<Aula> Aulas
+        {
+            get { return new ReadOnlyCollection<Aula>(aulas); }
+        }
+
+        public IList<Aluno> Alunos
+        {
+            get { return new ReadOnlyCollection<Aluno>(alunos.ToList()); }
+        }
+
+        public string Nome
+        {
+            get { return nome; }
+            set { nome = value; }
+        }
+
+        public string Instrutor
+        {
+            get { return instrutor; }
+            set { instrutor = value; }
+        }
+
+        public void Adiciona(Aula aula)
+        {
+            aulas.Add(aula);
+        }
+
+        public void Matricula(Aluno aluno)
+        {
+            alunos.Add(aluno);
+        }
+
+        public bool EstaMatriculado(Aluno aluno)
+        {
+            return alunos.Contains(aluno);
+        }
+
+    }
+}
