24e505947ebe5ad7edecdd50ec60545d97d3f1ef
gilvan <g.r.almeida@live.com>
2020-09-13 03:56:41 -0300

    Usando Set Dentro do Modelo

---
+++ SetNoModelo/SetNoModelo/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace SetNoModelo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Curso csharpColecoes = new Curso("C# Coleções", "Marcelo Oliveira");
+
+            csharpColecoes.Adiciona(new Aula("Trabalhando com Listas", 21));
+            csharpColecoes.Adiciona(new Aula("Criando uma Aula", 20));
+            csharpColecoes.Adiciona(new Aula("Modelando com Coleções", 24));
+
+            Aluno a1 = new Aluno("Vanessa Tonini", 34672);
+            Aluno a2 = new Aluno("Ana Losnak", 5617);
+            Aluno a3 = new Aluno("Rafael Nercessian", 17645);
+
+            csharpColecoes.Matricula(a1);
+            csharpColecoes.Matricula(a2);
+            csharpColecoes.Matricula(a3);
+
+            Console.WriteLine("Imprimindo os alunos matriculados");
+
+            foreach (var aluno in csharpColecoes.Alunos)
+            {
+                Console.WriteLine(aluno);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
