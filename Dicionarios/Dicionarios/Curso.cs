6596216eeee19f188c80ba33a79c0df4e2b64437
gilvan <g.r.almeida@live.com>
2020-09-13 04:40:12 -0300

    Introdução a dicionários

---
+++ Dicionarios/Dicionarios/Curso.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+
+namespace Dicionarios
+{
+    public class Curso
+    {
+        private IDictionary<int, Aluno> dicionarioAlunos = new Dictionary<int, Aluno>();
+        private ISet<Aluno> alunos = new HashSet<Aluno>();
+        private IList<Aula> aulas;
+        private string nome;
+        private string instrutor;
+
+        public Curso(string nome, string instrutor)
+        {
+            this.nome = nome;
+            this.instrutor = instrutor;
+            aulas = new List<Aula>();
+        }
+
+        public IList<Aula> Aulas
+        {
+            get { return new ReadOnlyCollection<Aula>(aulas); }
+        }
+
+        public IList<Aluno> Alunos
+        {
+            get { return new ReadOnlyCollection<Aluno>(alunos.ToList()); }
+        }
+
+        public string Nome
+        {
+            get { return nome; }
+            set { nome = value; }
+        }
+
+        public string Instrutor
+        {
+            get { return instrutor; }
+            set { instrutor = value; }
+        }
+
+        public void Adiciona(Aula aula)
+        {
+            aulas.Add(aula);
+        }
+
+        public void Matricula(Aluno aluno)
+        {
+            alunos.Add(aluno);
+            dicionarioAlunos.Add(aluno.NumeroMatricula, aluno);
+        }
+
+        public bool EstaMatriculado(Aluno aluno)
+        {
+            return alunos.Contains(aluno);
+        }
+
+        public Aluno BuscaMatriculado(int numeroMatricula)
+        {
+            // return dicionarioAlunos[numeroMatricula];
+            // Lança excessão KeyNotFoundException se chave não for encontrada
+
+            dicionarioAlunos.TryGetValue(numeroMatricula, out Aluno aluno);
+
+            return aluno;
+        }
+    }
+}
---
+++ Dicionarios/Dicionarios/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Dicionarios
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Curso csharpColecoes = new Curso("C# Coleções", "Marcelo Oliveira");
+
+            csharpColecoes.Adiciona(new Aula("Trabalhando com Listas", 21));
+            csharpColecoes.Adiciona(new Aula("Criando uma Aula", 20));
+            csharpColecoes.Adiciona(new Aula("Modelando com Coleções", 24));
+
+            Aluno a1 = new Aluno("Vanessa Tonini", 34672);
+            Aluno a2 = new Aluno("Ana Losnak", 5617);
+            Aluno a3 = new Aluno("Rafael Nercessian", 17645);
+
+            csharpColecoes.Matricula(a1);
+            csharpColecoes.Matricula(a2);
+            csharpColecoes.Matricula(a3);
+
+            Console.WriteLine("Imprimindo os alunos matriculados");
+
+            foreach (var aluno in csharpColecoes.Alunos)
+            {
+                Console.WriteLine(aluno);
+            }
+
+            Console.WriteLine($"O aluno a1 {a1.Nome} está matriculado?");
+            Console.WriteLine(csharpColecoes.EstaMatriculado(a1));
+
+            Aluno tonini = new Aluno("Vanessa Tonini", 34672);
+            Console.WriteLine("Tonini está matriculada? " + csharpColecoes.EstaMatriculado(tonini));
+
+            Console.WriteLine("a1 == a Tonini?");
+            Console.WriteLine(a1 == tonini);
+            Console.WriteLine(a1.Equals(tonini));
+
+            Console.Clear();
+
+            Console.WriteLine("Quem é o aluno com matrícula 5617?");
+
+            Aluno aluno5617 = csharpColecoes.BuscaMatriculado(5617);
+
+            Console.WriteLine("aluno5617: " + aluno5617);
+
+            // Um dicionário permite associar uma chave (no caso, matrícula)
+            // a um valor (o aluno)
+
+            Console.WriteLine("Quem é o aluno 5618?");
+
+            try
+            {
+                Console.WriteLine("aluno5618: " + csharpColecoes.BuscaMatriculado(5618));
+            }
+            catch (KeyNotFoundException e)
+            {
+                Console.WriteLine(e.Message);
+                // The given key was not present in the dictionary.
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
