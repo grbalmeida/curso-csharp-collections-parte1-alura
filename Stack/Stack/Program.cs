a19636f8c9619c79a3f552e072001b226e912d51
gilvan <g.r.almeida@live.com>
2020-09-13 20:20:14 -0300

    Stack

---
+++ Stack/Stack/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Stack
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var navegador = new Navegador();
+
+            navegador.NavegarPara("google.com");
+            navegador.NavegarPara("caelum.com.br");
+            navegador.NavegarPara("alura.com.br");
+
+            navegador.Anterior();
+            navegador.Anterior();
+            navegador.Anterior();
+            navegador.Anterior();
+
+            navegador.Proximo();
+            navegador.Proximo();
+            navegador.Proximo();
+            navegador.Proximo();
+            navegador.Proximo();
+
+            navegador.Anterior();
+            navegador.Anterior();
+            navegador.Anterior();
+
+            Console.ReadLine();
+        }
+    }
+
+    class Navegador
+    {
+        private readonly Stack<string> historicoAnterior = new Stack<string>();
+        private readonly Stack<string> historicoProximo = new Stack<string>();
+        private string atual = "vazia";
+
+        public Navegador()
+        {
+            Console.WriteLine($"Página atual: {atual}");
+        }
+
+        public void NavegarPara(string url)
+        {
+            historicoAnterior.Push(atual);
+            atual = url;
+            Console.WriteLine($"Página atual: {atual}");
+        }
+
+        public void Anterior()
+        {
+            // Método Pop lança InvalidOperationException se a pilha estiver vazia
+            
+            if (historicoAnterior.Any())
+            {
+                historicoProximo.Push(atual);
+                atual = historicoAnterior.Pop();
+                Console.WriteLine($"Página atual: {atual}");
+            }
+        }
+
+        public void Proximo()
+        {
+            if (historicoProximo.Any())
+            {
+                historicoAnterior.Push(atual);
+                atual = historicoProximo.Pop();
+                Console.WriteLine($"Página atual: {atual}");
+            }
+        }
+    }
+}
---
+++ Stack/Stack/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Stack")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Stack")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("6dc7fed9-ac43-4878-a3dd-d43e8db5bcf8")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
